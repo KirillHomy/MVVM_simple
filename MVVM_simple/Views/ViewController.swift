1c4d8395fd6d43dec3fbbc0a106890b77d18b50a
Kirill Khomicevich <khomicevichkirill@icloud.com>
2023-04-01 19:43:36 +0300

    Added SwiftLint and updated code

--- MVVM_simple/Views/ViewController.swift
+++ MVVM_simple/Views/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     // MARK: - Private variables
     private var viewModel = ViewModel()
